0c00bc42e0f0b08ea48d9e391d6f24cdb7960903
xkkhy <784457618@qq.com>
2022-06-14 23:10:22 +0800

    [MOD] 增加 gorm 示例文件

---
+++ g2gorm/examples/g2gorm/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	"go.uber.org/zap"
+	"gorm.io/gorm/logger"
+
+	"github.com/dijkvy/geocentric/g2gorm"
+	"github.com/dijkvy/geocentric/g2gorm/config"
+	"github.com/dijkvy/geocentric/log"
+	lc "github.com/dijkvy/geocentric/log/config"
+)
+
+var (
+	configPath = flag.String("config", "g2gorm/examples/g2gorm/configs/config.toml", "配置文件路径")
+)
+
+type Config struct {
+	LoggerConfig lc.ZapConfig        `toml:"logger_config"`
+	DBConfig     config.EngineConfig `toml:"db_config"`
+}
+
+type UserInfo struct {
+	ID        int64
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time
+}
+
+type g2Hook struct {
+}
+
+type anyKey int
+
+var ak anyKey
+
+// Fire 回调钩子函数
+func (c *g2Hook) Fire(e g2gorm.Entry) (err error) {
+	ctx := e.Context()
+	if ctx.Value(ak) == nil {
+		return nil
+	}
+	return e.AppendField(zap.String("trace", fmt.Sprintf("%v", ctx.Value(ak))))
+}
+
+func (c *g2Hook) Levels() (levels []logger.LogLevel) {
+
+	return append(levels, logger.Info, logger.Warn, logger.Warn, logger.Error)
+}
+
+func main() {
+
+	flag.Parse()
+	var conf Config
+	var err error
+	if _, err = toml.DecodeFile(*configPath, &conf); err != nil {
+		panic(err)
+	}
+	fmt.Println(conf.DBConfig.String())
+	fmt.Println(conf.LoggerConfig.String())
+	var helper g2gorm.DBHelper
+	var zl *zap.Logger
+	if zl, err = log.NewZapLogger(&conf.LoggerConfig); err != nil {
+		panic(err)
+	}
+	defer zl.Sync()
+
+	if helper, err = g2gorm.NewDBHelper(&conf.DBConfig,
+		g2gorm.WithLogger(
+			g2gorm.NewLoggerInterface(
+				g2gorm.NewWriter(zl, &g2Hook{} /*注册回调钩子函数*/),
+				logger.Config{
+					SlowThreshold: time.Duration(conf.DBConfig.GetSlowThreshold()) * time.Millisecond,
+					LogLevel:      g2gorm.Warn,
+				},
+			),
+		),
+	); err != nil {
+		panic(err)
+	}
+
+	// 清空表格
+	ctx := context.TODO()
+	helper.WithContext(context.TODO()).Exec("truncate  user_info")
+
+	// 向表格中插入数据
+	helper.WithContext(context.WithValue(ctx, ak, rand.ExpFloat64())).Table("user_info").Create(&UserInfo{CreatedAt: time.Now(), DeletedAt: time.Now(), UpdatedAt: time.Now()})
+	// 触发慢查询
+	_ = helper.WithContext(context.WithValue(ctx, ak, rand.ExpFloat64())).Exec("select sleep(4);").Error
+
+}
--- g2gorm/g2_logger_writer_impl.go
+++ g2gorm/g2_logger_writer_impl.go
@@ -3,28 +3,28 @@ package g2gorm
 import (
 	"context"
 	"fmt"
-	"os"
 
 	"go.uber.org/zap"
+	"gorm.io/gorm/logger"
 )
 
-func NewWriter(logger *zap.Logger, hook ...Hook) (w Writer) {
-	return &writerImpl{logger: logger, hook: hook}
+func NewWriter(logger *zap.Logger, optHooks ...Hook) (w Writer) {
+	var hooks = make(Hooks, 0)
+	for _, h := range optHooks {
+		for _, level := range h.Levels() {
+			hooks[level] = append(hooks[level], h)
+		}
+	}
+	return &writerImpl{logger: logger, hooks: hooks}
 }
 
 type writerImpl struct {
 	logger *zap.Logger
-	hook   []Hook
+	hooks  Hooks
 }
 
-func (c *writerImpl) Printf(ctx context.Context, format string, values ...interface{}) {
+func (c *writerImpl) Printf(ctx context.Context, level logger.LogLevel, format string, values ...interface{}) {
 	var e = newEntry(ctx)
-	for _, h := range c.hook {
-
-		if err := h.Fire(e); err != nil {
-			fmt.Fprintf(os.Stderr, "failed to fire  %s", err)
-			break
-		}
-	}
+	c.hooks.Fire(level, e)
 	c.logger.Info(fmt.Sprintf(format, values...), e.GetFields()...)
 }
