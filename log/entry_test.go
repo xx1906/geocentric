6daca895496bd39a744031cc14ef1f231408eb9f
xkkhy <784457618@qq.com>
2022-06-15 22:04:10 +0800

    [MOD] 调整 zap 生成的代码

--- log/entry_test.go
+++ log/entry_test.go
@@ -1,1 +1,16 @@
 package log
+
+import (
+	"context"
+	"testing"
+
+	"go.uber.org/zap"
+)
+
+func TestZapLoggerEntry_AppendField(t *testing.T) {
+	var entry Entry = newZapLogEntry(context.TODO())
+	entry.AppendField(zap.String("test", "test"))
+	if len(entry.GetFields()) != 1 {
+		t.Error("not ok")
+	}
+}
--- log/g2gorm_logger.go
+++
@@ -1,6 +0,0 @@
-package log
-
-// G2LoggerHelper gorm 日志接口
-type G2LoggerHelper interface {
-	Printf(format string, args ...interface{})
-}
