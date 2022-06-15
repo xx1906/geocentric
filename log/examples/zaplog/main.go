5e9ef80b358c58541a100635d3188d7859ea0978
xkkhy <784457618@qq.com>
2022-06-15 22:57:16 +0800

    [MOD] 重构 FieldLogger 接口

--- log/examples/zaplog/main.go
+++ log/examples/zaplog/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/BurntSushi/toml"
+	"github.com/google/uuid"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 
@@ -20,6 +21,21 @@ import (
 var (
 	cfgPath = flag.String("config", "log/examples/zaplog/configs/zaplog.toml", "zaplog config path")
 )
+
+type key int
+
+func (c key) Levels() (lvs []zapcore.Level) {
+	return []zapcore.Level{zapcore.DebugLevel, zapcore.InfoLevel, zapcore.WarnLevel, zapcore.ErrorLevel, zapcore.PanicLevel}
+}
+
+func (c key) Fire(e log.Entry) (err error) {
+	if e.Context().Value(_key) == nil {
+		return err
+	}
+	return e.AppendField(zap.Any("tr", e.Context().Value(_key)))
+}
+
+var _key key
 
 func main() {
 	flag.Parse()
@@ -46,19 +62,20 @@ func main() {
 	logger = logger.WithOptions(zap.WithCaller(true), zap.AddCallerSkip(2))
 	defer logger.Sync()
 	opts := make([]log.Option, 0)
-	opts = append(opts, log.AddHook(&injectAppName{AppName: "demo"}))
+	opts = append(opts, log.AddHook(&injectAppName{AppName: "demo"}), log.AddHook(key(0)))
 	helper := log.NewZapHelper(logger, opts...)
-	for i := 0; i < 100000000; i++ {
+	ctx := context.TODO()
+	for i := 0; i < 1000; i++ {
 
-		helper.WithContext(context.TODO()).Debug("debug")
-		helper.WithContext(context.TODO()).Info("info")
-		helper.WithContext(context.TODO()).Warn("warn")
-		helper.WithContext(context.TODO()).Error("error")
+		helper.WithContext(context.WithValue(ctx, _key, uuid.New().String())).Debug("debug")
+		helper.WithContext(context.WithValue(ctx, _key, uuid.New().String())).Info("info")
+		helper.WithContext(context.WithValue(ctx, _key, uuid.New().String())).Warn("warn")
+		helper.WithContext(context.WithValue(ctx, _key, uuid.New().String())).Error("error")
 
-		helper.WithContext(context.TODO()).Debugf("debug  time:%s", time.Now())
-		helper.WithContext(context.TODO()).Infof("info  time:%s", time.Now())
-		helper.WithContext(context.TODO()).Warnf("info  time:%s", time.Now())
-		helper.WithContext(context.TODO()).Errorf("error  time:%s", time.Now())
+		helper.WithContext(context.WithValue(ctx, _key, uuid.New().String())).Debug("debug  ", zap.Time("now", time.Now()))
+		helper.WithContext(context.WithValue(ctx, _key, uuid.New().String())).Info("info ", zap.Time("now", time.Now()))
+		helper.WithContext(context.WithValue(ctx, _key, uuid.New().String())).Warn("info ", zap.Time("now", time.Now()))
+		helper.WithContext(context.WithValue(ctx, _key, uuid.New().String())).Error("error ", zap.Time("now", time.Now()))
 	}
 }
 
